b108a14bf61750c74f668280bd35c56d47fb1818
Sebastien Lefevre <sebastienlfv.pro@gmail.com>
2025-11-17 10:09:25 +0100

    fix: dropdown delay is fixed

--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import "./dropdown.scss";
@@ -10,6 +10,7 @@ interface DropdownProps {
 
 const Dropdown: React.FC<DropdownProps> = ({ title, description }) => {
   const [open, setOpen] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
 
   const handleToggle = () => {
     setOpen(!open);
@@ -24,16 +25,23 @@ const Dropdown: React.FC<DropdownProps> = ({ title, description }) => {
           className={`chevron-icon${open ? " open" : ""}`}
         />
       </button>
-      <div className={`dropdown-description${open ? " open" : ""}`}>
-        {Array.isArray(description) ? (
-          <ul>
-            {description.map((item, idx) => (
-              <li key={idx}>{item}</li>
-            ))}
-          </ul>
-        ) : (
-          description
-        )}
+      <div
+        className="dropdown-description"
+        style={{
+          maxHeight: open ? contentRef.current?.scrollHeight : 0,
+        }}
+      >
+        <div ref={contentRef} className="dropdown-content">
+          {Array.isArray(description) ? (
+            <ul>
+              {description.map((item, idx) => (
+                <li key={idx}>{item}</li>
+              ))}
+            </ul>
+          ) : (
+            description
+          )}
+        </div>
       </div>
     </div>
   );
